11b77dd94c4ed243d9b53145a77732b4a62b1433
Akilan Anbu <akilananb@gmail.com>
2018-12-24 19:13:16 +0530

    added proper heroku url

--- src/client/App.js
+++ src/client/App.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-//components
+// components
 import BookList from './components/BookList';
 import AddBook from './components/AddBook';
 
-//apollo client setup
+// apollo client setup
 const client = new ApolloClient({
   uri:
     process.env.NODE_ENV === 'production'
-      ? 'https://graphql-react-apollo.herokuapp.com/'
-      : 'http://localhost:4000/graphql'
+      ? 'https://graphql-react-apollo.herokuapp.com/graphql'
+      : 'http://localhost:4000/graphql',
 });
-class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <div className="App">
-          <h1>Reading List</h1>
-          <BookList />
-          <AddBook />
-        </div>
-      </ApolloProvider>
-    );
-  }
-}
+const App = () => (
+  <ApolloProvider client={client}>
+    <div className="App">
+      <h1>Reading List</h1>
+      <BookList />
+      <AddBook />
+    </div>
+  </ApolloProvider>
+);
 
 export default App;
